ce7e528308b16d9cd0e8da8d6fd0caf58c3c659d
NTurchi <nicolas.turchi@hotmail.fr>
2018-04-12 13:38:06 +0200

    fix

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var ss = require("./services/socketServer");
 
 // global variable
 var app = express();
+app.use(function (request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 var port = 4000;
 // Launch socket server
 ss.socketServer(app.listen(port));
